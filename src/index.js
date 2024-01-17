7ed18aeff26e33b21a65c52ad9c1f2342037ed36
Sunpreet Kaur <ssunpreet555@gmail.com>
2024-01-17 21:04:23 +0530

    completed published mail events

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 
+const amqplib=require("amqplib");
+const { EmailService } = require('./services');
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel= await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue" , async(data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail("flightsservice1@gmail.com", object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 const mailsender = require('./config/email-config');
@@ -8,7 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-
+      await connectQueue();
+      console.log('queue is up');
 });
