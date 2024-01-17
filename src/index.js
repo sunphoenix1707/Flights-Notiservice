9c843c226fbe9f4e03bbe6048675a48385eddf3c
Sunpreet Kaur <ssunpreet555@gmail.com>
2024-01-17 11:22:25 +0530

    added create ticket api

--- src/index.js
+++ src/index.js
@@ -8,17 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-   try {
-    const response = await mailsender.sendMail({
-        from: ServerConfig.GMAIL_EMAIL,
-        to: 'ssunpreet555@gmail.com',
-        subject: 'IS the service working?',
-        text: 'Yes It is Working,Testimg whether email sent successfully or not'
-    });
-    console.log(response);
-   } catch (error) {
-    console.log(error);
-   }
+
 });
